471160bc0bea1b66a2c3c7727fcaf463a35f9a36
Per Skarin <per.skarin@ericsson.com>
2018-01-12 12:30:17 +0100

    White space replace

--- pyqpgen/generate.m
+++ pyqpgen/generate.m
@@ -11,46 +11,46 @@ fprintf(fid, '#define SAMPLE_RATE %d\n',round(1/MPC.h));
 fprintf(fid, '#define HORIZON %d\n',MPC.N);
 fprintf(fid, '#define ADATA {');
 for i=1:size(MPC.Adyn, 1)
-	for j=1:size(MPC.Adyn, 2)
-		if i == 1 && j == 1
-			fprintf(fid, '%e', MPC.Adyn(i,j));
-		else
-			fprintf(fid, ', %e', MPC.Adyn(i,j));
-		end
-	end
+  for j=1:size(MPC.Adyn, 2)
+    if i == 1 && j == 1
+      fprintf(fid, '%e', MPC.Adyn(i,j));
+    else
+      fprintf(fid, ', %e', MPC.Adyn(i,j));
+    end
+  end
 end
 fprintf(fid, '}\n');
 fprintf(fid, '#define BDATA {');
 for i=1:size(MPC.Bdyn, 1)
-	for j=1:size(MPC.Bdyn, 2)
-		if i == 1 && j == 1
-			fprintf(fid, '%e', MPC.Bdyn(i,j));
-		else
-			fprintf(fid, ', %e', MPC.Bdyn(i,j));
-		end
-	end
+  for j=1:size(MPC.Bdyn, 2)
+    if i == 1 && j == 1
+      fprintf(fid, '%e', MPC.Bdyn(i,j));
+    else
+      fprintf(fid, ', %e', MPC.Bdyn(i,j));
+    end
+  end
 end
 fprintf(fid, '}\n');
 fprintf(fid, '#define QDATA {');
 for i=1:size(MPC.Q, 1)
-	for j=1:size(MPC.Q, 2)
-		if i == 1 && j == 1
-			fprintf(fid, '%e', MPC.Q(i,j));
-		else
-			fprintf(fid, ', %e', MPC.Q(i,j));
-		end
-	end
+  for j=1:size(MPC.Q, 2)
+    if i == 1 && j == 1
+      fprintf(fid, '%e', MPC.Q(i,j));
+    else
+      fprintf(fid, ', %e', MPC.Q(i,j));
+    end
+  end
 end
 fprintf(fid, '}\n');
 fprintf(fid, '#define RDATA {');
 for i=1:size(MPC.R, 1)
-	for j=1:size(MPC.R, 2)
-		if i == 1 && j == 1
-			fprintf(fid, '%e', MPC.R(i,j));
-		else
-			fprintf(fid, ', %e', MPC.R(i,j));
-		end
-	end
+  for j=1:size(MPC.R, 2)
+    if i == 1 && j == 1
+      fprintf(fid, '%e', MPC.R(i,j));
+    else
+      fprintf(fid, ', %e', MPC.R(i,j));
+    end
+  end
 end
 fprintf(fid, '}\n');
 fclose(fid);
